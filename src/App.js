5cad0bb105b596f6589af315af93d49c1fe44822
Prince <princeacharya66@gmaail.com>
2023-06-20 14:59:24 +0545

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import Home from '../src/home/Home'
+import * as React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from '../src/home/Home';
 import './App.css';
+
 function App() {
   return (
-    
-
-<Home />
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/Dashboard" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
